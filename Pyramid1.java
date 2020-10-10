744cc30cd39c24047a5f080965b5844ed870f858
Kalina-K-K <72562377+Kalina-K-K@users.noreply.github.com>
2020-10-10 23:21:40 +0300

    Add files via upload

---
+++ Pyramid1.java
@@ -0,0 +1,20 @@
+package edu;
+import java.util.Scanner;
+public class Pyramid1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = n; i>0; i--) {
+			for(int j = i; j>=1; j--) {
+				System.out.print(j);
+			}
+			for(int j = 2; j<=i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
